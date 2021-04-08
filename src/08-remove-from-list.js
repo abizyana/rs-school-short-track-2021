e4e64dc0dde122042bb20d8cab7ee26f15adcfdf
abizyana <abi3yana@gmail.com>
2021-04-08 16:00:46 +0200

    feat: remove from list add

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,18 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  const list = l;
+  let temp = l;
+
+  while (temp.next) {
+    if (temp.value === k) {
+      temp.value = temp.next.value;
+      temp.next = temp.next.next;
+    }
+    temp = temp.next;
+  }
+  return list;
 }
 
 module.exports = removeKFromList;
